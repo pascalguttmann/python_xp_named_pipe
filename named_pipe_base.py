7e0ddc4fe4408dd5facd7face90798cc4ecb28c8
pascalguttmann <github@pascal-guttmann.de>
2024-12-21 07:42:58 +0100

    add abstracted structure

--- named_pipe_base.py
+++ named_pipe_base.py
@@ -1,32 +1,75 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
 from typing import TypeVar
 
 
 class NamedPipeBase(ABC):
-    @abstractmethod
-    def __init__(self, *args, **kwargs) -> None:
-        """Initialize the SPI bus master to allow data transfer to slave devices"""
+    def __init__(self, path: str) -> None:
+        """Initialize namedPipe"""
+        self._path = path
+        return
 
-    def __enter__(self):
-        pass
+    def __enter__(self) -> NamedPipeBase:
+        return self.mkfifo()
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
-        pass
+        return self.unlink()
 
-    def mkfifo():
-        pass
+    @abstractmethod
+    def mkfifo(self) -> NamedPipeBase:
+        """Create the namedPipe"""
 
-    def open():
-        pass
-
-    def close():
-        pass
-
-    def read():
-        pass
-
-    def write():
-        pass
+    @abstractmethod
+    def unlink(self) -> None:
+        """Unlink / remove the namedPipe"""
 
 
-NamedPipe = TypeVar("NamedPipe", bound=NamedPipe)
+NamedPipe = TypeVar("NamedPipe", bound=NamedPipeBase)
+
+
+class PipeEndBase(ABC):
+    def __enter__(self):
+        """Return the PipeEnd for read or write"""
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        """Close the PipeEnd"""
+        return self._close()
+
+    @abstractmethod
+    def _open(self, named_pipe: NamedPipeBase, mode: str) -> None:
+        """Open the named pipe in read xor write mode"""
+
+    @abstractmethod
+    def _close(self) -> None:
+        """Close the namedPipe"""
+
+
+PipeEnd = TypeVar("PipeEnd", bound=PipeEndBase)
+
+
+class WritePipeEndBase(PipeEndBase):
+    def __init__(self, path: str) -> None:
+        """Open the NamedPipe in write mode"""
+        return super().__init__(path, "w")
+
+    @abstractmethod
+    def write(self, string: str):
+        """Writes to the pipe"""
+
+
+WritePipeEnd = TypeVar("WritePipeEnd", bound=WritePipeEndBase)
+
+
+class ReadPipeEndBase(PipeEndBase):
+    def __init__(self, path: str) -> None:
+        """Open the NamedPipe in read mode"""
+        return super().__init__(path, "r")
+
+    @abstractmethod
+    def read(self) -> str:
+        """Reads from the pipe"""
+
+
+ReadPipeEnd = TypeVar("ReadPipeEnd", bound=ReadPipeEndBase)
