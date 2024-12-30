7e76c27bb64d20d918d3d8fe36b3fb3c4192b797
pascalguttmann <github@pascal-guttmann.de>
2024-12-30 05:09:30 +0100

    fix Base64DatagrammeEncoderDecoder datatype from bytearray to bytes

--- base64_encoder_decoder.py
+++ base64_encoder_decoder.py
@@ -6,18 +6,18 @@ from typing import Callable
 class Base64DatagrammeEncoderDecoder:
     def __init__(
         self,
-        read_func: Callable[[], bytearray],
-        write_func: Callable[[bytearray], None],
+        read_func: Callable[[], bytes],
+        write_func: Callable[[bytes], None],
     ) -> None:
         self.read_func = read_func
         self.write_func = write_func
         self._datagrammes = []
         self._partial_enc_datagramme = bytes()
 
-    def _datagrammes_fifo_put(self, datagramme: bytearray) -> None:
+    def _datagrammes_fifo_put(self, datagramme: bytes) -> None:
         self._datagrammes.append(datagramme)
 
-    def _datagrammes_fifo_pop(self) -> bytearray:
+    def _datagrammes_fifo_pop(self) -> bytes:
         return self._datagrammes.pop(0)
 
     def _datagrammes_fifo_is_empty(self) -> bool:
@@ -26,12 +26,12 @@ class Base64DatagrammeEncoderDecoder:
         else:
             return False
 
-    def _set_partial_enc_datagramme(self, datagramme: bytearray) -> None:
+    def _set_partial_enc_datagramme(self, datagramme: bytes) -> None:
         self._partial_enc_datagramme = datagramme
 
-    def _pop_partial_enc_datagramme(self) -> bytearray:
+    def _pop_partial_enc_datagramme(self) -> bytes:
         partial_enc_datagramme = self._partial_enc_datagramme
-        self._set_partial_enc_datagramme(bytearray())
+        self._set_partial_enc_datagramme(bytes())
         return partial_enc_datagramme
 
     def _read_datagrammes_to_fifo(self) -> None:
@@ -43,15 +43,15 @@ class Base64DatagrammeEncoderDecoder:
             del enc_datagrammes[-1]
 
         for enc_dg in enc_datagrammes:
-            dec_dg = bytearray(base64.b64decode(enc_dg.rstrip(b"\x00")))
+            dec_dg = bytes(base64.b64decode(enc_dg.rstrip(b"\x00")))
             self._datagrammes_fifo_put(dec_dg)
         return None
 
-    def write(self, data: bytearray) -> None:
-        encoded_data = bytearray(base64.b64encode(data))
+    def write(self, data: bytes) -> None:
+        encoded_data = bytes(base64.b64encode(data))
         self.write_func(encoded_data + b"\x00")
 
-    def read(self) -> bytearray:
+    def read(self) -> bytes:
         while self._datagrammes_fifo_is_empty():
             self._read_datagrammes_to_fifo()
         return self._datagrammes_fifo_pop()
